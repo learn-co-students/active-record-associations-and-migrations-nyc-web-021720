d45d9fd692bc452d756bb4b429dd028b82dc665f
lucassherwin <lksherwin@gmail.com>
2020-03-05 09:29:09 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,19 @@
 class Genre < ActiveRecord::Base
     has_many :songs
     has_many :artists, through: :songs
+
+    def song_count
+        self.songs.length
+    end
+
+    def artist_count
+        self.artists.length
+    end
+
+    def all_artist_names
+        # self.artists.map do |obj|
+        #     obj.name
+        # end
+        self.artists.map(&:name)
+    end
 end
